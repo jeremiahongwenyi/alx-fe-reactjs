8073a7467291d65c8bbf2d325ef7404a9f36930f
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-31 22:55:09 +0300

    question 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and Photography" />
       <Footer />
     </>
   );
