7e2d5bc59d3ded92d703e016c00d87b39a9f526a
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-31 22:30:15 +0300

    correction question 1

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,9 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+
 
 function App() {
   const [count, setCount] = useState(0)
