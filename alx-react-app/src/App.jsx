0e91bfc3776477c8819a919654a6dd940a74d20f
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-31 22:44:02 +0300

    question 2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,23 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   
-   <WelcomeMessage />
-
-  )
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
