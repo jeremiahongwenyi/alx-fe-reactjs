3b1eab1f6fe90dcd0032ddc9fe1041b43c0a845b
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-09-07 21:57:06 +0300

    add new directory

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{ backgroundColor: "navy", color: "white", padding: "20px" }}
+    >
+      <p style={{ textAlign: "center" }}>
+        <small>
+          <em>© 2023 City Lovers</em>
+        </small>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
