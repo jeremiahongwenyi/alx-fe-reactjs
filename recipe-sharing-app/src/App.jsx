1252c38073407ff782a448a22b3e3bafa31482b4
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-09-14 22:44:24 +0300

    initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from "../src/components/AddRecipeForm";
+import RecipeList from "../src/components/RecipeList";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <AddRecipeForm></AddRecipeForm>
+      <RecipeList></RecipeList>
+    </>
+  );
+}
+
+export default App;
