c7affc3f7d2e70b54b8bd6c4f7cc8db81688c83f
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-09-28 23:01:17 +0300

    question 1 and 2

--- tailwind-react-integration/src/main.jsx
+++ tailwind-react-integration/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import UserProfile from "./components/UserProfile.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserProfile></UserProfile>
+  </StrictMode>
+);
