8db668eec98bd003974f8c6bbd11e144b7968a15
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-10-29 12:10:46 +0300

    question 1 correction

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PostsComponent from './components/PostsComponent'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PostsComponent from "./components/PostsComponent";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = new QueryClient();
 
   return (
     <>
-    <PostsComponent></PostsComponent>
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent></PostsComponent>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
